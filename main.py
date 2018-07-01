76c029930fdfc5d70e46ce8977db564de060f794
sjang42 <jexjang92@gmail.com>
2018-07-02 03:14:08 +0900

    Add main func

--- main.py
+++ main.py
@@ -40,3 +40,45 @@ def find_shortest_straight(location_standard, locations_hospital, num_find=3):
     shortest = [heapq.heappop(heap)[1] for i in range(num_find)]
     return shortest
 
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    # set hyper parameters
+    path_to_offices = './resources/location_offices.xlsx'
+    path_to_hospitals = './resources/hira_all_addr_w_coord.xlsx'
+    num_find = 3
+    out_name = 'test.xlsx'
+
+    # load excel file and convert to list of dictionary
+    offices = excel_to_dict(path_to_offices)
+    hospitals = excel_to_dict(path_to_hospitals)
+
+    # set workbook to save
+    wb = Workbook()
+    ws = wb.active
+
+    len_offices = len(offices)
+    for i, office in enumerate(offices):
+        shortest = find_shortest_straight(office, hospitals, num_find)
+        for num in range(num_find):
+            office['OBJECTID'] = i + 1
+            office['hname' + str(num + 1)] = hospitals[shortest[num]]['hname']
+            office['htype' + str(num + 1)] = hospitals[shortest[num]]['htype']
+            office['Y_hosp' + str(num + 1)] = hospitals[shortest[num]]['Y']
+            office['X_hosp' + str(num + 1)] = hospitals[shortest[num]]['X']
+            office['dist_straight'
+                   + str(num)] = hospitals[shortest[num]]['straight_distance']
+
+        if i == 0:
+            ws.append(list(office.keys()))
+
+        if (i + 1) % 20 == 0 or i + 1 == len_offices:
+            print('find offices [{}/{}]'.format(i + 1, len_offices))
+            wb.save('test.xlsx')
+
+        ws.append(list(office.values()))
+
+    wb.save('test.xlsx')
+    elapsed_time = time.time() - start_time
+    print('Took {:.03f} seconds'.format(elapsed_time))
