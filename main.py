91bd353978970a5fb071976559500a08f82e268e
sjang42 <jexjang92@gmail.com>
2018-07-15 23:32:05 +0900

    add out file name

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ def find_shortest_straight(location_standard, locations_hospital, num_find=3):
     heap = []
     total = len(locations_hospital)
     for i, hospital in enumerate(locations_hospital):
+        if not hospital['X']:
+            if print_None:
+                print('wrong!', hospital)
+            continue
         distance = haversine((location_standard['Y'], location_standard['X']),
                              (hospital['Y'], hospital['X']))
         hospital['straight_distance'] = distance
@@ -47,38 +51,52 @@ if __name__ == '__main__':
     # set hyper parameters
     path_to_offices = './resources/location_offices.xlsx'
     path_to_hospitals = './resources/hira_all_addr_w_coord.xlsx'
+
+    all_hospitals = ['hira_subcategory_가정의학과.xlsx']
+    dirname = './resources/'
     num_find = 3
-    out_name = 'test.xlsx'
 
-    # load excel file and convert to list of dictionary
-    offices = excel_to_dict(path_to_offices)
-    hospitals = excel_to_dict(path_to_hospitals)
+    for hosname in all_hospitals:
+        path_to_hospitals = dirname + hosname
+        print(path_to_hospitals)
 
-    # set workbook to save
-    wb = Workbook()
-    ws = wb.active
+        out_name = 'shortest_' + path_to_hospitals.split('_')[-1]
+        print(out_name)
 
-    len_offices = len(offices)
-    for i, office in enumerate(offices):
-        shortest = find_shortest_straight(office, hospitals, num_find)
-        for num in range(num_find):
-            office['OBJECTID'] = i + 1
-            office['hname' + str(num + 1)] = hospitals[shortest[num]]['hname']
-            office['htype' + str(num + 1)] = hospitals[shortest[num]]['htype']
-            office['Y_hosp' + str(num + 1)] = hospitals[shortest[num]]['Y']
-            office['X_hosp' + str(num + 1)] = hospitals[shortest[num]]['X']
-            office['dist_straight' + str(num + 1)] = hospitals[shortest[num]][
-                'straight_distance']
+        # load excel file and convert to list of dictionary
+        offices = excel_to_dict(path_to_offices)
+        hospitals = excel_to_dict(path_to_hospitals)
 
-        if i == 0:
-            ws.append(list(office.keys()))
+        # set workbook to save
+        wb = Workbook()
+        ws = wb.active
 
-        if (i + 1) % 20 == 0 or i + 1 == len_offices:
-            print('find offices [{}/{}]'.format(i + 1, len_offices))
-            wb.save('test.xlsx')
+        len_offices = len(offices)
+        for i, office in enumerate(offices):
+            print_None = False
+            if i == 1:
+                print_None = True
+            shortest = find_shortest_straight(office, hospitals, num_find)
+            for num in range(num_find):
+                office['OBJECTID'] = i + 1
+                office['hname' + str(num + 1)] = hospitals[shortest[num]]['병원약국명']
+                office['htype' + str(num + 1)] = hospitals[shortest[num]]['htype']
+                office['Y_hosp' + str(num + 1)] = hospitals[shortest[num]]['Y']
+                office['X_hosp' + str(num + 1)] = hospitals[shortest[num]]['X']
+                office['dist_straight' + str(num + 1)] = hospitals[shortest[num]][
+                    'straight_distance']
 
-        ws.append(list(office.values()))
+            if i == 0:
+                ws.append(list(office.keys()))
 
-    wb.save('test.xlsx')
+            if (i + 1) % 20 == 0 or i + 1 == len_offices:
+                print('find offices [{}/{}]'.format(i + 1, len_offices))
+                wb.save(out_name)
+
+            ws.append(list(office.values()))
+
+            wb.save(out_name)
+        print('saved to :', out_name)
+        print('')
     elapsed_time = time.time() - start_time
     print('Took {:.03f} seconds'.format(elapsed_time))
