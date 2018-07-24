f54f7b4fb6db0f403fdf66ad878d799cfce0a89c
sjang42 <jexjang92@gmail.com>
2018-07-24 13:08:01 +0900

    Save output to excel file

--- find_directions.py
+++ find_directions.py
@@ -11,32 +11,40 @@ in_dirname = './straight/'
 straight = os.listdir(in_dirname)
 
 num_find = 3
-# todo : key 제한 걸고 loop 돌기
 
 for f in straight:
     if not is_excel(f):
         continue
 
     fname = os.path.join(in_dirname, f)
-    print(fname)
-    offices = excel_to_dict(fname, mute=False)
+    print('input:', fname)
+    out_name = 'gmap_direction_' + str(fname).split('_')[-1]
+    print('output:', out_name)
 
     wb = Workbook()
     ws = wb.active
 
+    offices = excel_to_dict(fname, mute=True)
+
+    len_offices = len(offices)
     for i, office in enumerate(offices):
         origin = (office['Y'], office['X'])
-        print(office)
         for num in range(num_find):
             destination = (office['Y_hosp' + str(num + 1)], office['X_hosp' + str(num + 1)])
             direction_result = gmap.get_direction(origin, destination)
             direction_dict = result2dict(direction_result, num + 1)
-            print(direction_dict)
-        if i == 1:
-            break
+            office.update(direction_dict)
 
-    # todo: 저장 전 순서 바꿔주기
-    # todo: 저장하기
+        # Add header in the first row
+        if i == 0:
+            ws.append(list(office.keys()))
 
-    break
+        ws.append(list(office.values()))
+        if (i + 1) % 50 == 0 or i + 1 == len_offices:
+            print('find offices [{}/{}]'.format(i + 1, len_offices))
+            wb.save(out_name)
 
+    wb.save(out_name)
+    print('saved to :', out_name)
+
+print('\n-----done-----\n')
