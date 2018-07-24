75aa5e09a6aa6204607f890a5c5ebfad3690becd
sjang42 <jexjang92@gmail.com>
2018-07-24 13:05:33 +0900

    Reflect final options
    - distance as meter, durations as second
    - add moving duration
    - departure time changed to future
    - fix typo

--- gmap_direction.py
+++ gmap_direction.py
@@ -14,6 +14,7 @@ def result2dict(direction_result, num_hosp):
     if direction_result['status'] != 'OK':
         ret['total_distance' + num_hosp] = -1
         ret['total_duration' + num_hosp] = -1
+        ret['moving_duration' + num_hosp] = 'None'
         ret['end_locations' + num_hosp] = 'None'
         ret['travel_modes' + num_hosp] = 'None'
         ret['distances' + num_hosp] = 'None'
@@ -23,20 +24,19 @@ def result2dict(direction_result, num_hosp):
         return ret
 
     route = direction_result['routes'][0]['legs'][0]
-    total_distance = route['distance']['value'] / 1000  # km
-    total_duration = route['duration']['value'] / 1000  # minute
+    total_distance = route['distance']['value']  # m
+    total_duration = route['duration']['value']  # s
 
     steps = route['steps']
     end_locations = []
     travel_modes = []
     distances = []
     durations = []
+    moving_duration = 0
 
     for step in steps:
-        distance = step['distance']['value'] / 1000  # km
-        duration = step['duration']['value'] / 60  # minute
-        distance = float_trim(distance)
-        duration = float_trim(duration)
+        distance = step['distance']['value']  # m
+        duration = step['duration']['value']  # s
         end_location = tuple(step['end_location'].values())
 
         travel_mode = step['travel_mode']
@@ -45,11 +45,13 @@ def result2dict(direction_result, num_hosp):
 
         distances.append(distance)
         durations.append(duration)
+        moving_duration += duration
         end_locations.append(end_location)
         travel_modes.append(travel_mode)
 
     ret['total_distance' + num_hosp] = total_distance
     ret['total_duration' + num_hosp] = total_duration
+    ret['moving_duration' + num_hosp] = str(moving_duration)
     ret['end_locations' + num_hosp] = list2str(end_locations)
     ret['travel_modes' + num_hosp] = list2str(travel_modes)
     ret['distances' + num_hosp] = list2str(distances)
@@ -63,12 +65,12 @@ class GmapDirection:
     def __init__(self, key):
         self.api_url = 'https://maps.googleapis.com/maps/api/directions/json'
         self.departure_time = datetime.strptime(
-            "2018-07-16 08:00",
+            "2018-09-03 06:00",
             "%Y-%m-%d %H:%M").timestamp()  # 2018-07-09 08:00
         self.departure_time = int(self.departure_time)
 
         self.mode = 'transit'
-        self.transit_mode = ['bus', 'subway']
+        self.transit_mode = 'bus|subway'
         self.region = 'kr'
         self.language = 'ko'
 
@@ -79,7 +81,7 @@ class GmapDirection:
             'key': self.key,
             'region': self.region,
             'mode': self.mode,
-            'trainsit_mode': self.transit_mode,
+            'transit_mode': self.transit_mode,
             'language': self.language,
             'departure_time': self.departure_time
         }
